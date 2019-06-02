28739ff570ea9c15d6df3e0c415279108518aa94
Emre DEĞER <emredeger16@gmail.com>
2019-06-02 21:22:43 +0300

    chore(app): added auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { UsersModule } from './user/user.module';
 import { BookModule } from './book/book.module';
-import { AuthService } from './auth/auth.service';
-import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    "type": "postgres",
-    "host": "localhost",
-    "port": 5432,
-    "username": "postgres",
-    "password": "changeme",
-    "database": "nest",
-    "entities": [__dirname + "/**/*.entity{.ts,.js}"],
-    "synchronize": true
-  }),
-  UsersModule,
-  BookModule],
-  providers: [AuthService],
-  controllers: [AuthController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'changeme',
+      database: 'nest',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+    BookModule,
+    AuthModule,
+  ],
+  providers: [],
+  controllers: [],
 })
 export class ApplicationModule {
   constructor(private readonly connection: Connection) {}
