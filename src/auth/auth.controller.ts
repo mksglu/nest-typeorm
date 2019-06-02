109186b819252b261f96a0abf0c26655a425a3b9
Emre DEĞER <emredeger16@gmail.com>
2019-06-02 21:19:34 +0300

    fix: changed jsonwebtoken to @nestjs/jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,33 +6,27 @@ import {
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
+import { UsersService } from '../user/user.service';
+import { AuthService } from './auth.service';
 import { User } from '../user/user.entity';
-import { Repository } from 'typeorm';
-// import {Response} from 'express'
+import { LoginDto } from './dto/login.dto';
+
 @Controller('auth')
 export class AuthController {
   constructor(
-    private readonly usersService: Repository<User>,
-    private readonly authService: Repository<any>,
-  ) {
-    console.log('auth controller');
-  }
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('login')
-  async loginUser(@Response() res: Response, @Body() body: User) {
-    if (!(body && body.email && body.password)) {
-      throw new HttpException(
-        {
-          status: HttpStatus.FORBIDDEN,
-          error: 'unauthorize',
-        },
-        403,
-      );
-    }
-    const { email } = body;
+  async loginUser(@Response() res: Response, @Body() body: LoginDto) {
+    const { email, password } = body;
     const user = await this.usersService.findOne({ where: { email } });
-    if(user) {
 
+    if (user) {
+      // TODO: Compare Hash
     }
+
+    return res.json({});
   }
 }
