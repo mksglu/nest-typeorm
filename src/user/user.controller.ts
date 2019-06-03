c304414d2136f0c3e617322f68dce83b7d21a258
Emre DEĞER <emredeger16@gmail.com>
2019-06-03 22:11:58 +0300

    fix(user controller): added RolesGuard and Roles decorator (#5).

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,13 +3,16 @@ import { UsersService } from './user.service';
 import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { createUserDto } from './dto/create-user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { Roles } from '../common/decorators/roles.decorator';
+import { RolesGuard } from '../common/guards/roles.guard';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UsersService) {}
 
   @Get('/')
-  @UseGuards(AuthGuard())
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @Roles('admin')
   getAllUsers(): Promise<User[]> {
     return this.userService.getAllUsers();
   }
