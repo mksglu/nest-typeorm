71b231aa411113eb3acf3c0ab8702d2121d38261
Emre DEĞER <emredeger16@gmail.com>
2019-06-03 22:11:04 +0300

    chore(): added roles guard (#5)

---
+++ src/common/guards/roles.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class RolesGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    const hasRole = () =>
+      user.roles.some(role => !!roles.find(item => item === role));
+
+    return user && user.roles && hasRole() ? super.canActivate(context) : false;
+  }
+}
